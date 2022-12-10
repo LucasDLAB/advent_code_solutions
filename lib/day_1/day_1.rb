2c3d4b69af96752163e25b6414d9894c02a070e5
Lucas Borges <diasb.trabalho@gmail.com>
2022-12-10 17:01:07 -0300

    Corrige erro de nilclass

--- lib/day_1/day_1.rb
+++ lib/day_1/day_1.rb
@@ -9,6 +9,8 @@ def get_empty_calories_index(calories)
 end
 
 def get_three_highers(calories, elf_calories)
+  p calories
+  p elf_calories
   highers = []
   beginning = 0
 
@@ -23,10 +25,14 @@ end
 
 
 if __FILE__ == $PROGRAM_NAME
-  calories = File.open('lib/day_1/day_1_input.txt')
-  index ||= get_empty_calories_index(calories.readlines
-    .map!{ |calorie| calorie.delete("\n")})
+  calories = File.open('lib/day_1/day_1_input.txt').readlines
+
+  index = get_empty_calories_index(
+    calories.map!{ |calorie| calorie.delete("\n")}
+  )
+
   highers = get_three_highers(calories.map(&:to_i), index)
+
   p "#{highers.max} is the highest number of calories"
   p "#{highers.sum} is the sum of the three highest number of calories"
 end
